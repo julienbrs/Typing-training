4fdb3dc556471bdcdbbf1ecb18e15f599c28c5fd
Julien Bourseau <Julien.Bourseau@grenoble-inp.org>
2022-07-27 00:45:54 +0200

    effectif, clean et design a continuer

--- wpm_training.py
+++ wpm_training.py
@@ -1,41 +1,54 @@
 # pylint: disable=no-member
+from matplotlib.pyplot import text
 import pygame
 import pygame.freetype
 pygame.freetype.init()
 import sys
 import os
+from itertools import cycle
+import dico
 
 pygame.font.init()
 pygame.mixer.init()
 FPS = 240
 
-WIN_SIZE = WIDTH, HEIGHT = 1200, 1000
+WIN_SIZE = WIDTH, HEIGHT = 1500, 844
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Typing Training")
 
 
 SOUND_SELECT_MENU = pygame.mixer.Sound(
     os.path.join('assets', 'sound_effects', 'menu.wav'))
+SOUND_KEYPAD = pygame.mixer.Sound(
+    os.path.join('assets', 'sound_effects', 'keypad.mp3')
+)
+SOUND_KEYPAD_WRONG = pygame.mixer.Sound(
+    os.path.join('assets', 'sound_effects', 'wrong.wav')
+)
 
 
 # LOADING SCREEN
-IMG_BACKGROUND_LOADING_SCREEN   = pygame.image.load(os.path.join("./assets/ciel.jpg"))
-IMG_TEST                        = pygame.image.load(os.path.join("./assets/ciel2.jpg"))
+IMG_BACKGROUND_LOADING_SCREEN   = pygame.image.load(os.path.join("./assets/yellow_duck_wp.jpg"))
+IMG_TEST                        = pygame.image.load(os.path.join("./assets/yellow_wallpaper.jpg"))
 
 
 
 
 WHITE   = (255, 255, 255)
+LIGHT_BLUE = (167, 190, 211)
+YELLOW_ORANGE = (253, 184, 19)
+LIGHT_PURPLE = (153, 93, 206)
 GREY    = (125, 125, 125)
 LIGHT_GREY = (175, 175, 175)
 
-FONT_MENU_START = pygame.font.SysFont("comicsans", 55)
-FONT_MENU_LOADING_SCREEN = pygame.font.SysFont("comicsans", 120)
+FONT_HELVETICA  = pygame.font.SysFont("helvetica", 40)
+FONT_ARABOTO_50  = pygame.font.SysFont("haraboto", 50)
+FONT_ARABOTO_80  = pygame.font.SysFont("haraboto", 80)
 
-text_menu_welcome       = FONT_MENU_LOADING_SCREEN.render("TYPING TRAINING", 1, LIGHT_GREY)
-text_menu_start_game    = FONT_MENU_START.render("Start a new session", 1, GREY)
-text_menu_dictionnary   = FONT_MENU_START.render("Dictionnary", 1, GREY)
-text_menu_leaderboard   = FONT_MENU_START.render("Leaderboard", 1, GREY)
+text_menu_welcome       = FONT_ARABOTO_80.render("TYPING TRAINING", 1, LIGHT_PURPLE)
+text_menu_start_game    = FONT_ARABOTO_50.render("Start a new session", 1, LIGHT_PURPLE)
+text_menu_dictionnary   = FONT_ARABOTO_50.render("Dictionnary", 1, LIGHT_PURPLE)
+text_menu_leaderboard   = FONT_ARABOTO_50.render("Progression", 1, LIGHT_PURPLE)
 
 
 # todo: mettre "progression au lieu de leaderboard"
@@ -56,7 +69,7 @@ class LinkedText():
     def draw(self, surface, text):
         "draw the text with line or not if selected"
         if self == MENU_SELECTED:
-            new_size = (text.get_width() *1.8, text.get_height()*1.8)
+            new_size = (text.get_width() *1, text.get_height()*1)
             new_text = pygame.transform.scale(text, new_size)
             surface.blit(new_text, (self.x - new_text.get_width()/2,
                      self.y - new_text.get_height()/2))
@@ -70,9 +83,9 @@ class LinkedText():
             surface.blit(text, (self.x - text.get_width()/2,
                      self.y - text.get_height()/2))
 
-start_game  = LinkedText(WIDTH /2, HEIGHT /2, None, None)
-dictionnary  = LinkedText(WIDTH /2, HEIGHT /1.5, None, start_game)
-leaderboard = LinkedText(WIDTH /2, HEIGHT / 1.3, start_game, dictionnary)
+start_game  = LinkedText(WIDTH /2, HEIGHT /5, None, None)
+dictionnary  = LinkedText(WIDTH /2, HEIGHT /3.5, None, start_game)
+leaderboard = LinkedText(WIDTH /2, HEIGHT / 2.7, start_game, dictionnary)
 dictionnary.next_text = leaderboard
 start_game.next_text, start_game.prev_text = dictionnary, leaderboard
 MENU_SELECTED = start_game
@@ -83,10 +96,10 @@ def draw_menu(text_blink, text_scroll, surface):
 
     surface.blit(pygame.transform.scale(IMG_BACKGROUND_LOADING_SCREEN, WIN_SIZE), (0,0))
 
-    text_blink = (text_blink + 1) % 1000
-    if text_blink <= 500 :
+    text_blink = (text_blink + 1) % 350
+    if text_blink <= 200 :
         surface.blit(text_menu_welcome,((WIDTH - text_menu_welcome.get_width())/2,
-                    (HEIGHT/3 - text_menu_welcome.get_height())/2))
+                    (HEIGHT/5 - text_menu_welcome.get_height())/2))
 
     start_game.draw(surface, text_menu_start_game)
     leaderboard.draw(surface, text_menu_leaderboard)
@@ -103,12 +116,6 @@ def draw_menu(text_blink, text_scroll, surface):
     pygame.display.update()
     return text_blink, text_scroll
 
-def draw_text_target(text, surface):
-    text_target   = FONT_MENU_START.render(text, 1, GREY)
-    #text_size = (text.get_width() *1.8, text.get_height()*1.8)
-    #new_text = pygame.transform.scale(text, new_size)
-    surface.blit(text_target, (WIDTH/2, HEIGHT/2))
-
 
 def wait(text_target, index):
     while True:
@@ -118,7 +125,9 @@ def wait(text_target, index):
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.unicode == text_target[index]:
+                    pygame.mixer.Sound.play(SOUND_KEYPAD)
                     return 2
+                pygame.mixer.Sound.play(SOUND_KEYPAD_WRONG)
                 return 1
 
 def main():
@@ -130,7 +139,8 @@ def main():
     text_blink = 0
     text_scroll = None
     clock = pygame.time.Clock()
-    text_target = "Hello! That this the text that you have to write"
+    data = cycle(dico.main("temp.txt", "dictionnary.txt")) #todo: shuffle
+    current_text = next(data)
     while APP_RUN:
         clock.tick(FPS)
         while run_menu:
@@ -177,28 +187,49 @@ def main():
                 pygame.display.update()
 
                 current_index = 0
-                font = pygame.freetype.Font(None, 50) #todo revoir principe font
+                font = pygame.freetype.Font(os.path.join("fonts", "Helvetica.ttf"), 50)
                 font.origin = True
                 font_height = font.get_sized_height()
                 M_ADV_X = 4 #todo ??? Madv_x
                     # let's calculate how big the entire line of text is
-                text_surf_rect = font.get_rect(text_target)
+                text_surf_rect = font.get_rect(current_text)
+                text_surf_rect.size = (text_surf_rect.size[0]*1.1, text_surf_rect.size[1]) #todo plus élégant
+                text_surf_background_rect = font.get_rect(current_text)
+                text_surf_background_rect.size = (text_surf_rect.width * 1.15, text_surf_rect.height * 1.7)
                 # in this rect, the y property is the baseline
                 # we use since we use the origin mode
                 baseline = text_surf_rect.y
+
                 # now let's create a surface to render the text on
                 # and center it on the screen
                 text_surf = pygame.Surface(text_surf_rect.size)
+                text_surf_background = pygame.Surface(text_surf_background_rect.size)
+
+                text_surf_background_rect.center = WIN.get_rect().center
                 text_surf_rect.center = WIN.get_rect().center
                 # calculate the width (and other stuff) for each letter of the text
-                metrics = font.get_metrics(text_target)
+                metrics = font.get_metrics(current_text)
 
                 #todo gérer fin de phrase
 
-            WIN.fill('black')
-            text_surf.fill('white')
+            if current_index >= len(current_text) - 1:
+                # if the sentence is complete, let's prepare the
+                # next surface
+                current_index = 0
+                current_text = next(data)
+                text_surf_rect = font.get_rect(current_text)
+                baseline = text_surf_rect.y
+
+                text_surf = pygame.Surface(text_surf_rect.size)
+                text_surf_rect.center = text_surf_background_rect.center
+                metrics = font.get_metrics(current_text)
+                test_letter = 0
+
+            WIN.blit(pygame.transform.scale(IMG_BACKGROUND_LOADING_SCREEN, WIN_SIZE), (0,0))
+            #text_surf_background.fill('white')
+            text_surf.fill(YELLOW_ORANGE)
             x = 0
-            for (idx, (letter, metric)) in enumerate(zip(text_target, metrics)):
+            for (idx, (letter, metric)) in enumerate(zip(current_text, metrics)):
                 # select the right color
                 if idx == current_index:
                     if test_letter == 0:        # no test atm
@@ -212,17 +243,18 @@ def main():
                 elif idx < current_index:
                     color = 'green'
                 else:
-                    color = 'black'
+                    color = (96, 81, 109)
                 # render the single letter
                 font.render_to(text_surf, (x, baseline), letter, color)
                 # and move the start position
                 x += metric[M_ADV_X]
-
+            #text_surf_background.set_alpha(90)
+            #WIN.blit(text_surf_background, text_surf_background_rect)
             WIN.blit(text_surf, text_surf_rect)
             pygame.display.update()
 
             pygame.event.clear()
-            test_letter = wait(text_target, current_index)
+            test_letter = wait(current_text, current_index)
 
 
 
