16b358b959056211b5e2b81afb9b362469165ba1
julienbrs <julien.bourseau@gmail.com>
2023-08-18 21:13:57 +0200

    dictionnary init

--- src/utils/manage_dictionnary.py
+++ src/utils/manage_dictionnary.py
@@ -6,25 +6,17 @@ DICTIONARY_PATH = os.path.join(BASE_DIR, 'dictionnary.txt')
 NEW_WORDS_PATH = os.path.join(BASE_DIR, 'new_words.txt')
 TEMP_GAME_DICT_PATH = os.path.join(BASE_DIR, 'temp_game_dictionnary.txt')
 
+
 def extract_words_from_file(path_to_file):
     """ Extracts words from a file separated by spaces """
     with open(path_to_file, 'r') as file:
         return file.read().split()
 
 
-def add_words_to_file(filename, words):
-    """ Adds words to a file separated by spaces """
-    with open(filename, 'a') as file:
-        file.write(" ".join(words))
-
-
 def create_list_for_game(params, source=DICTIONARY_PATH, dest=TEMP_GAME_DICT_PATH):
     """ Creates a list of words for the game from the source file and writes it to dest, using params."""
     # Extract words from source
     words = extract_words_from_file(source)
-    print("we are creating")
-    print("params", params)
-    print("Initial words:", len(words))
 
     # Check if we use capital letters
     if not params['capital_letters']:
@@ -83,29 +75,12 @@ def add_new_word_list_to_dest(source=NEW_WORDS_PATH, dest=DICTIONARY_PATH):
     with open(dest, 'w') as file:
         file.write('\n'.join(combined_words))
 
-    return combined_words
-
-
-def add_word_to_dest(word, dest=DICTIONARY_PATH):
-    """Adds a word to the dest file, filter duplicates"""
-    # Extract words from dest
-    already_used_words = extract_words_from_file(dest)
-
-    # Combine and de-duplicate words
-    combined_words = list(set([word] + already_used_words))
-
-    # Write the organized words back to dest
-    with open(dest, 'w') as file:
-        file.write('\n'.join(combined_words))
+    # Erase content of source file
+    with open(source, 'w') as file:
+        file.write('')
 
     return combined_words
 
 
 if __name__ == "__main__":
-    params = {
-        'capital_letters': True,
-        'accents': True,
-        'punctuation': True,
-        'numbers': True
-    }
     add_new_word_list_to_dest()
